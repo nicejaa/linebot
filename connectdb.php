918d0a1bdba019d723420a1c4f9850b6c4899470
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-10 11:36:22 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -3,5 +3,6 @@ $con = new mysqli("us-cdbr-east-03.cleardb.com","b53b313730e59d","0ba0f852","her
 if($con->connect_error){
     die("connect Fail :".$con->connect_error);
 }
+    echo "Connected successfully";
   $con->query("SET NAMES UTF8");
  ?>
