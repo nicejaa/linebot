edb0c56df3d86d1dcd38ae690ce0df53a46ec4ab
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-10 14:58:03 +0700

    Update bot-checktoken.php

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -19,14 +19,14 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $reply_message = '';
         $reply_token = $event['replyToken'];
-
+        $message = $events['events'][0]['message']['text'];
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+//             'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
+        
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
         echo "Result: ".$send_result."\r\n";
