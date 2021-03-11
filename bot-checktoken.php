fb7ba4419c65458d8017c4302cabbd65ed209e88
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-11 13:20:35 +0700

    Update bot-checktoken.php

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -1,7 +1,11 @@
+
 <?php
 
 include 'connectdb.php';
+echo '<script type="text/javascript">';
+  echo 'liff.getAccessToken();</script>';
 
+$sql = "SELECT * FROM user Where User_Token = ";
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'gMEkhBcxQF0jT72jVrQZfZ8N3hU3gKmS1F3rjRZmeUuVn5ccNfh4AJQxzQ0L1nFJyOSLgc1vBCxX/Sk7r8cAJtEts0vTaK9Z7MA8Xff4Kgx1JoEj+KtyR+kn1j80SZFMus8th1QNI4vMSKHI5vRGbwdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = 'aa92cfff7cdb8b56bdb472b9102bba32';
