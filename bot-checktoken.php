86ea6498d40d584ced410a8f2f8433eed80168e8
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-11 14:28:47 +0700

    Update bot-checktoken.php

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -48,7 +48,8 @@ if ( sizeof($request_array['events']) > 0 ) {
                     ];
         
              $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-             $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+             $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, 'สวัสดีค้าบ');
+               
            }
       
   
