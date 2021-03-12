960659bf42a20f2d285c5a57c0714b5c7abba5ce
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-12 14:17:15 +0700

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -2,7 +2,7 @@
 require "vendor/autoload.php";
 $access_token = 'gMEkhBcxQF0jT72jVrQZfZ8N3hU3gKmS1F3rjRZmeUuVn5ccNfh4AJQxzQ0L1nFJyOSLgc1vBCxX/Sk7r8cAJtEts0vTaK9Z7MA8Xff4Kgx1JoEj+KtyR+kn1j80SZFMus8th1QNI4vMSKHI5vRGbwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'aa92cfff7cdb8b56bdb472b9102bba32';
-$idPush = 'U306d5bcfb4110b6ca721033437ad55a7'
+$idPush = 'U306d5bcfb4110b6ca721033437ad55a7';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
