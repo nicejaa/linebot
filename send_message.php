7682f489b08744098e966d91aee21126176af5b3
nicejaa <58319397+nicejaa@users.noreply.github.com>
2021-03-12 14:18:37 +0700

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,5 +1,5 @@
 <?php
-require "vendor/autoload.php";
+
 $access_token = 'gMEkhBcxQF0jT72jVrQZfZ8N3hU3gKmS1F3rjRZmeUuVn5ccNfh4AJQxzQ0L1nFJyOSLgc1vBCxX/Sk7r8cAJtEts0vTaK9Z7MA8Xff4Kgx1JoEj+KtyR+kn1j80SZFMus8th1QNI4vMSKHI5vRGbwdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'aa92cfff7cdb8b56bdb472b9102bba32';
 $idPush = 'U306d5bcfb4110b6ca721033437ad55a7';
